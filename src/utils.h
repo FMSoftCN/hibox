2085e9afcfd5ca8c7662ef0afd2e9647d0fd9edc
Vincent Wei <vincent@minigui.org>
2020-10-28 11:19:21 +0800

    add API for SHA1

--- src/utils.h
+++ src/utils.h
@@ -253,4 +253,21 @@ static inline unsigned long cbuf_size(int order)
 void *cbuf_alloc(unsigned int order);
 void cbuf_free(void *ptr, unsigned int order);
 
+/* SHA1 */
+
+#include <sys/types.h>
+#include <stdint.h>
+
+// From http://www.mirrors.wiretapped.net/security/cryptography/hashes/sha1/sha1.c
+
+typedef struct {
+  uint32_t state[5];
+  uint32_t count[2];
+  uint8_t buffer[64];
+} SHA1_CTX;
+
+extern void sha1_init (SHA1_CTX * context);
+extern void sha1_update (SHA1_CTX * context, uint8_t * data, uint32_t len);
+extern void sha1_finalize (uint8_t digest[20], SHA1_CTX * context);
+
 #endif
